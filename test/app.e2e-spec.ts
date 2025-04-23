e54905d6b9c7a30fba8ad4908337f72c91d08c30
fridem mulli <2382018@unai.edu>
2025-04-23 10:14:43 +0700

    perbaharui

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,7 +7,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  beforeEach(async () => {      
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
